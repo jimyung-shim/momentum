6637c7a36499e6fd435d244e54a4ebe1e3cedf37
jimyung <sjm77773@naver.com>
2025-03-31 22:56:35 +0900

    add welcome statements

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app=express();
 const port=3000;
 
+app.get('/',(req,res)=>{
+    res.send('Welcome to animal sound api!');
+});
+
 app.listen(port, ()=>{
     console.log('Local hosting at '+port);
 });
